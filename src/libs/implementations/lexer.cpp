61132466705e63fef6060fdae90a8ddd5024f343
Matheus Leonel de Andrade <matheus.leoenl161@gmail.com>
2024-04-14 16:36:27 -0300

    Versao quase funcional, nao pega o ultimo caracter

--- src/libs/implementations/lexer.cpp
+++ src/libs/implementations/lexer.cpp
@@ -4,12 +4,10 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-const int FINAL_STATE = 23;
-
 const int TRANSITION_TABLE[24][24] = {
 /*    ,  ;  (  )  +  -  /  *  <  =  >  "  \  {  }  L  $  D  _  .  e  E \n   estado final 1 para aceitacão
       0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22   23 */
-    { 1, 2, 3, 4, 5, 5, 5, 5, 6, 8, 9,10,-1,13,-1,16,17,18,-1,-1,10,10, 0,    0},
+    { 1, 2, 3, 4, 5, 5, 5, 5, 6, 8, 9,10,-1,13,-1,16,17,18,-1,-1,16,16, 0,    0},
     {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,    1},
     {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,    1},
     {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,    1},
@@ -19,12 +17,12 @@ const int TRANSITION_TABLE[24][24] = {
     {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,    1},
     {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,    1},
     {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,    1},
-    {10,10,10,10,10,10,10,10,10,10,10,12,11,10,10,10,10,10,10,10,10,10,10,    0},
-    {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,10,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,    0},
-    {10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,    1},
-    {13,13,13,13,13,13,13,13,13,13,13,13,15,13,14,13,13,13,13,13,13,13,13,    0},
+    {10,10,10,10,10,10,10,10,10,10,10,12,11,10,10,10,10,10,10,10,10,10,-1,    0},
+    {10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,-1,    0},
     {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,    1},
-    {13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,    0},
+    {13,13,13,13,13,13,13,13,13,13,13,13,15,13,14,13,13,13,13,13,13,13,-1,    0},
+    {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,    1},
+    {13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,-1,    0},
     {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,16,-1,16,16,-1,16,16,-1,    1},
     {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,    1},
     {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,18,-1,19,21,21,-1,    1},
@@ -34,6 +32,8 @@ const int TRANSITION_TABLE[24][24] = {
     {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,22,-1,-1,-1,-1,-1,    1},
     {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,22,-1,-1,-1,-1,-1,    0}
 };
+
+const int FINAL_STATE = 23;
 
 const char TOKEN_CLASS[12][11] = {
     {"VIR"},
@@ -87,8 +87,12 @@ int main(int argc,char *argv[]){
             fseek(file, file_position, SEEK_SET);
             ch = fgetc(file);
             restore_pos = false;
-            printf("entrou aqui");
         }
+
+        while ((' ' == ch || '\t' == ch || '\n' == ch || '\r' == ch || '\f' == ch || '\v' == ch) && STATE == 0 && restore_pos != true) {
+            ch = fgetc(file);
+        }
+
         if(ch == '\\' and cont < 10){
             cont++;
             if (cont == 9)
@@ -97,8 +101,6 @@ int main(int argc,char *argv[]){
             }
             
         }
-
-        printf("character: |%c|\n", ch);
         
         file_position = ftell(file) - 1;
         
@@ -110,21 +112,28 @@ int main(int argc,char *argv[]){
         if(NEXT_STATE == -1){
             restore_pos = true;
 
+            buffer[i] = '\0';
+
+            // printf("\ntoken -1 & ch |%c|\n", ch);
             make_token(buffer, STATE, is_final(STATE));
             STATE = 0;
-            buffer[++i] = '\0';
             i = 0;
         }
         else if (NEXT_STATE == -2){
             buffer[i++] = ch;
-            buffer[i++] = '\0';
+            buffer[i] = '\0';
+
+            // printf("\ntoken -2 & ch |%d| & state: %d\n", ch, STATE);
             make_token(buffer, STATE, false);
+            STATE = 0;
             i = 0;
         }
         else{
             buffer[i++] = ch;
             STATE = NEXT_STATE;
         }
+        
+        // printf("character: |%c| || state: %d\n", ch, STATE);
     }
 
     fclose(file);
@@ -150,21 +159,64 @@ bool is_final(int STATE){
 
 void make_token(char *buffer, int STATE, bool valid){
     if(!valid){
-        printf("erro | %s\n", buffer);
+        printf("token: ERRO & |%s|\n", buffer);
     }
-    switch (STATE){
-    case 1:
-        printf("vir | %s\n", buffer);
-        break;
-    case 16:
-        printf("id | %s\n", buffer);
-        break;
-    case 17:
-        printf("eof | %s\n", buffer);
-        break;
-    default:
-        break;
+    else{
+        switch (STATE){
+        case 1:
+            printf("token: VIR & |%s|\n", buffer);
+            break;
+        case 2:
+            printf("token: PT_V & |%s|\n", buffer);
+            break;
+        case 3:
+            printf("token: AB_P & |%s|\n", buffer);
+            break;
+        case 4:
+            printf("token: FC_P & |%s|\n", buffer);
+            break;
+        case 5:
+            printf("token: OPM & |%s|\n", buffer);
+            break;
+        case 6:
+            printf("token: OPR & |%s|\n", buffer);
+            break;
+        case 7:
+            printf("token: RCB & |%s|\n", buffer);
+            break;
+        case 8:
+            printf("token: OPR & |%s|\n", buffer);
+            break;
+        case 9:
+            printf("token: OPR & |%s|\n", buffer);
+            break;
+        case 12:
+            printf("token: Lit & |%s|\n", buffer);
+            break;
+        case 14:
+            printf("token: Comentario & |%s|\n", buffer);
+            break;
+        case 16:
+            printf("token: ID & |%s|\n", buffer);
+            break;
+        case 17:
+            printf("token: EOF & |%s|\n", buffer);
+            break;
+        case 18:
+            printf("token: Num & |%s|\n", buffer);
+            break;
+        case 20:
+            printf("token: Num & |%s|\n", buffer);
+            break;
+        case 22:
+            printf("token: Num & |%s|\n", buffer);
+            break;
+        default:
+            printf("token: ERRO & |%s|\n", buffer);
+            break;
+        }   
     }
+   
 }
 
     // {"VIR"},
@@ -217,20 +269,16 @@ int get_symbol(char ch){
             return 13;
         case '}':
             return 14;
-        case 'L':
-            return 15;
         case '$':
             return 16;
-        case 'D':
-            return 17;
         case '_':
             return 18;
         case '.':
             return 19;
         case 'e':
-            return 20;
+            return 15;
         case 'E':
-            return 21;
+            return 15;
         case '\n':
             return 22;
         case '\r':
@@ -238,6 +286,10 @@ int get_symbol(char ch){
         case ' ':
             return 22;
         case '\t':
+            return 22;
+        case '\v':
+            return 22;
+        case '\f':
             return 22;
         default:
             return -2;
